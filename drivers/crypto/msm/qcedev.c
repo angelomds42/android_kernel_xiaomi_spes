8d75161f44158ce355043bb519aa636d6d3b87cb
AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
2019-11-05 13:14:31 +0100

    crypto/msm/qcedev: Fix mess in qcedev_ioctl code
    We use the sg_src variable in most of the IOCTLs and it's really
    useless to declare it every time in every case of the switch.
    Solve the indentation mess and move that variable up.
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -1783,6 +1783,7 @@ static inline long qcedev_ioctl(struct file *file,
 	struct qcedev_control *podev;
 	struct qcedev_async_req *qcedev_areq;
 	struct qcedev_stat *pstat;
+	struct scatterlist sg_src;
 
 	qcedev_areq = kzalloc(sizeof(struct qcedev_async_req), GFP_KERNEL);
 	if (!qcedev_areq)
@@ -1841,9 +1842,6 @@ static inline long qcedev_ioctl(struct file *file,
 		break;
 
 	case QCEDEV_IOCTL_SHA_INIT_REQ:
-		{
-		struct scatterlist sg_src;
-
 		if (copy_from_user(&qcedev_areq->sha_op_req,
 					(void __user *)arg,
 					sizeof(struct qcedev_sha_op_req))) {
@@ -1869,7 +1867,6 @@ static inline long qcedev_ioctl(struct file *file,
 			goto exit_free_qcedev_areq;
 		}
 		handle->sha_ctxt.init_done = true;
-		}
 		break;
 	case QCEDEV_IOCTL_GET_CMAC_REQ:
 		if (!podev->ce_support.cmac) {
@@ -1878,9 +1875,6 @@ static inline long qcedev_ioctl(struct file *file,
 		}
 		/* Fall-through */
 	case QCEDEV_IOCTL_SHA_UPDATE_REQ:
-		{
-		struct scatterlist sg_src;
-
 		if (copy_from_user(&qcedev_areq->sha_op_req,
 					(void __user *)arg,
 					sizeof(struct qcedev_sha_op_req))) {
@@ -1980,9 +1974,6 @@ static inline long qcedev_ioctl(struct file *file,
 		break;
 
 	case QCEDEV_IOCTL_GET_SHA_REQ:
-		{
-		struct scatterlist sg_src;
-
 		if (copy_from_user(&qcedev_areq->sha_op_req,
 					(void __user *)arg,
 					sizeof(struct qcedev_sha_op_req))) {
